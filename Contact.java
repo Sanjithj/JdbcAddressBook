ab6743ad3843cfc18396ddb49207daddcfdc52df
Sanjithj <sanjithj591997@gmail.com>
2022-10-11 15:36:04 +0530

    Added: contacts added

---
+++ Contact.java
@@ -0,0 +1,55 @@
+package com.bridgelabz;
+
+public class Contact {
+	private String firstName;
+    private String lastName;
+    private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private String city;
+    private String state;
+    private int zip;
+    private long phone;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    
+    public Contacts(String first_name, String last_name, String address, String city, String state, int zip, long phone, String email_id) {
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email_id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "First name is: "+firstName+"\nLast Name is: "+lastName+"\nAddress is: "+address+"\nCity name is: "+city+
+                "\nState is: "+state+"\nZip is: "+zip+"\nPhone Number is: "+phone+"\nEmail Id is: "+email+"\n";
+    }
+
+	public Object getZip() {
+		
+		return null;
+	}
+}
+
+}
